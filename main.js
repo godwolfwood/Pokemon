437778686f339861e1cd45260e8f33b32f8b2ec4
Juan <plascencia.juan831@gmail.com>
2017-07-25 15:16:43 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    for(var i = 1; i <= 151; i++){
+        $('.wrapper').append(
+            //<img src="http://pokeapi.co/media/img/1.png">
+            "<img src='http://pokeapi.co/media/img/" + i + ".png'>"
+        );
+    }
+});
